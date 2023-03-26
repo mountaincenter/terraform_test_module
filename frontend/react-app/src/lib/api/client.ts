84a2e16bf1aa68da66c5077b96ee740205ee3d99
mountaincenter <y.hiroyuki560730@gmail.com>
2023-03-26 19:29:00 +0900

    installed eslint and prettier

--- frontend/react-app/src/lib/api/client.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios"
-
-const client = axios.create({
-  baseURL: process.env.REACT_APP_NODE_ENV === "development" ? "http://localhost/api/v1" : "https://api.ymnk.fun/api/v1"
-})
-
-export default client
